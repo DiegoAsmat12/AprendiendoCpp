258cf68e9fa8651796526f1e88e153a1203abea4
diegoas1205 <diegoas1205@gmail.com>
2020-06-01 12:47:45 -0500

    Asignacion dinámica de memoria

--- SolucionCuadradoMagico/SolucionCuadradoMagico.cpp
+++ SolucionCuadradoMagico/SolucionCuadradoMagico.cpp
@@ -9,6 +9,16 @@ int main()
     int dimension;
     cin >> dimension;
     
+    int** arreglo = new int* [dimension];
+    for (int i = 0; i < dimension; i++) {
+        arreglo[i] = new int[dimension];
+    }
+    for (int i = 0; i < dimension; i++) {
+        for (int j = 0; j < dimension; j++) {
+            arreglo[i][j] = 0;
+            cout << arreglo[i][j] <<endl;
+        }
+    }
     printf("%d. x %d.",dimension,dimension);
 }
 
