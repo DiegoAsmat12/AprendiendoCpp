c8b833979e8face9a37b3180634fcab3fa0063dc
diegoas1205 <diegoas1205@gmail.com>
2020-06-01 11:38:17 -0500

    Variables, vectore, matrices funciones, entrada y salida, for y if

--- AprendiendoCpp_1/AprendiendoCpp_1.cpp
+++ AprendiendoCpp_1/AprendiendoCpp_1.cpp
@@ -1,10 +1,41 @@
 
 #include <iostream>
-using namespace std;
+#include <vector>
+using namespace std; //Gracias a STD no es necesario usar std::cout o std::endl
 
-
+static void Saludo();
+string Nombre;
 int main()
 {
-    std::cout << "Hello World!"<<endl;
-}
+    cin >> Nombre;
+    printf(Nombre.c_str());
+    vector<int> Vector = vector<int>(25);
+    int array[] = { 1,2,3,4 };
+    int matriz[3][2] = { {1,2},{2,3},{3,4} };
+    cout << "[ ";
+    for (int i=0; i < Vector.size(); i++) {
+        Vector[i] = i+1;
+        cout<< Vector[i]<< ", ";
+        if (i == Vector.size() - 1) {
+            cout << Vector[i] << "]"<<endl;
+        }
+    }
+    
 
+    int ValorEntero = 1;
+    for (int i = 0; i <= 10; i++) {
+        if (i >= 5) {
+            cout << "Hello World!"<<endl;
+        }
+        else {
+            std::cout << "Hello World!" << "hi, hi" << "\n" << "Hola broer" << std::endl;
+        }
+        
+    }
+    Saludo();
+    cout << ValorEntero;
+    return 0;
+}
+static void Saludo() {
+    cout << "Hi! ";
+}
