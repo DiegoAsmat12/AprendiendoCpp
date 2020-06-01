7b8196830f3822acabfba2a0a3a9e0e176a231c6
diegoas1205 <diegoas1205@gmail.com>
2020-06-01 12:24:41 -0500

    No se puede crear Matriz De Manera no dinámica

--- SolucionCuadradoMagico/SolucionCuadradoMagico.cpp
+++ SolucionCuadradoMagico/SolucionCuadradoMagico.cpp
@@ -1,13 +1,14 @@
 
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main()
-{
+{ 
+
     int dimension;
     cin >> dimension;
     
-
     printf("%d. x %d.",dimension,dimension);
 }
 
